ffa5a047458fa30cd5762b497d959445ff3200e3
RG <reece.goding@outlook.com.>
2024-12-02 22:05:38 +0000

    Day 2

---
+++ Day 2 - Santa's jumbled letters.sql
@@ -0,0 +1,25 @@
+SELECT
+    STRING_AGG(ANSI_Value, '') OVER (ORDER BY table_index, id) decoded_message
+FROM
+(
+    SELECT
+        1 table_index
+        ,id
+        ,CHR(value) ANSI_Value
+    FROM
+        public.letters_a
+
+    UNION
+
+    SELECT
+        2 table_index
+        ,id
+        ,CHR(value) ANSI_Value
+    FROM
+        public.letters_b
+) RawLetter
+WHERE
+    ANSI_Value ~ '[a-zA-Z !"''(),.:;?-]'
+ORDER BY
+    LENGTH(STRING_AGG(ANSI_Value, '') OVER (ORDER BY table_index, id)) DESC
+LIMIT 1;
